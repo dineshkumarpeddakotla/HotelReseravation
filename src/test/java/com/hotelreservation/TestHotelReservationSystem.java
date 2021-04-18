abece76b7b81dc1fffc40c178043dda03e7075d5
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-18 19:10:28 +0530

    ADDED: bestRatedHotel Method to Find Best Rated Hotel

--- src/test/java/com/hotelreservation/TestHotelReservationSystem.java
+++ src/test/java/com/hotelreservation/TestHotelReservationSystem.java
@@ -18,23 +18,10 @@ public class TestHotelReservationSystem {
 
         System.out.print(hotelName);
         System.out.println(" Price is $" +hotelPrice);
-        hotelReservationSystem.print();
 
         Assertions.assertEquals("Ridgewood", hotelName );
         Assertions.assertEquals(220, hotelPrice);
     }
-//
-//    @Test
-//    public void givenRangeOfDateShouldReturnCheapestHotelAtThatPeriod() {
-//        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
-//        hotelReservationSystem.addHotel();
-//
-//        String cheapestHotel = hotelReservationSystem.findCheapestHotel("10/09/2020", "11/09/2020");
-//
-//        System.out.println(cheapestHotel);
-//
-//        Assertions.assertEquals("Lakewood, Total Price = $220", cheapestHotel);
-//    }
 
     @Test
     public void whenWeekdayAndWeekendPricesIsAddedToHotelList(){
@@ -50,18 +37,6 @@ public class TestHotelReservationSystem {
     }
 
     @Test
-    public void givenRangeOfDatesReturnBestCheapestRatedHotelAtThatPeriod() {
-        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
-        hotelReservationSystem.addHotel();
-
-        Map.Entry<String, Hotel> cheapestHotel = hotelReservationSystem.findBestCheapestRatedHotel("11/09/2020", "12/09/2020");
-
-        System.out.println(cheapestHotel);
-
-        Assertions.assertEquals("Bridgewood=Hotel{hotelName='Bridgewood', totalPrice=200, rating=4}", cheapestHotel.toString());
-    }
-
-    @Test
     public void whenRatingOfHotelIsAddedToList(){
         HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
 
@@ -72,5 +47,29 @@ public class TestHotelReservationSystem {
 
         Assertions.assertEquals("Ridgewood", hotelName );
         Assertions.assertEquals(5, hotelPrice);
+    }
+
+    @Test
+    public void givenRangeOfDatesReturnBestCheapestRatedHotelAtThatPeriod() {
+        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+        hotelReservationSystem.addHotel();
+
+        Map.Entry<String, Hotel> cheapestHotel = hotelReservationSystem.findBestCheapestRatedHotel("11/09/2020", "12/09/2020");
+
+        System.out.println(cheapestHotel);
+
+        Assertions.assertEquals("Bridgewood=Hotel{hotelName='Bridgewood', totalPrice = $200, rating = 4}", cheapestHotel.toString());
+    }
+
+    @Test
+    public void givenRangeOfDatesReturnBestRatedHotelAtThatPeriod() {
+        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+        hotelReservationSystem.addHotel();
+
+        Map.Entry<String, Hotel> bestRatedHotel = hotelReservationSystem.findBestRatedHotel("11/09/2020", "12/09/2020");
+
+        System.out.println(bestRatedHotel);
+
+        Assertions.assertEquals("Ridgewood=Hotel{hotelName='Ridgewood', totalPrice = $370, rating = 5}", bestRatedHotel.toString());
     }
 }
